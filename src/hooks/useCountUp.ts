d4d05cdcbe600167d8f73c78988ccf1db91b693e
sunkyungnoh <cellin1637@gmail.com>
2022-06-23 05:39:19 +0900

    Feature : useCountUp 추가
    - _ref와 numbers를 반환하는 custom hook
    - 관찰되기 원하는 element의 ref에 _ref값을 넣어 viewport 이내의 요소가 나타났을 때 카운트업이 실행된다.
    - duration, easing 값을 추가로 넣어 사용할 수 있다.

---
+++ src/hooks/useCountUp.ts
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+import useIntersectionObserver from './useIntersectionObserver';
+
+interface propType {
+  duration?: number;
+  easing?: 'linear' | 'easeIn' | 'easeOut' | 'easeOutQuart';
+}
+
+const EASING = {
+  linear: (t: number) => t,
+  easeIn: (t: number) => t * t,
+  easeOut: (t: number) => t * (2 - t),
+  easeOutQuart: (t: number) => 1 - --t * t * t * t,
+  default: (t: number) => 1 - Math.exp(-t * 7),
+};
+
+export const useCountUp = (
+  numbers: number[],
+  { duration, easing }: propType
+) => {
+  const [count, setCount] = useState([...numbers]);
+  const _duration = duration ?? 2000;
+  const _easing = easing ?? 'default';
+  const frameDuration = 1000 / 60;
+  const totalFrames = Math.round(_duration / frameDuration);
+
+  const countUp = (num: number, index: number) => {
+    let frame = 0;
+    const counter = setInterval(() => {
+      frame++;
+      const progress = EASING[_easing](frame / totalFrames);
+      const currentCount = Math.ceil(num * progress);
+      setCount((prev) => {
+        const result = [...prev];
+        result.splice(index, 1, currentCount);
+        return result;
+      });
+      if (frame === totalFrames) {
+        clearInterval(counter);
+        return;
+      }
+    }, frameDuration);
+  };
+
+  const onIntersect: IntersectionObserverCallback = ([entry], observer) => {
+    if (entry.isIntersecting) {
+      count.forEach((num, i) => countUp(num, i));
+      observer.disconnect();
+    }
+  };
+
+  const { setTarget } = useIntersectionObserver({ onIntersect });
+
+  return {
+    numbers: count,
+    ref: setTarget,
+  };
+};
