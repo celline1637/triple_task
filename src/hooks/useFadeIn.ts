2814be607c15103cb3d4566fe1d39b52a7b0c1f2
sunkyungnoh <cellin1637@gmail.com>
2022-06-23 05:26:05 +0900

    Feature : useFadeIn 추가
    - _ref와 triggers를 반환하는 custom hook
    
    - 관찰되기 원하는 element의 ref에 _ref값을 넣어 viewport 여부를 알 수 있다.
    
    - useFadeIn의 인자는 fadeIn 효과가 적용될 element의 개수를 의미한다.
    - useFadeIn의 인자가 없을 경우, triggers는 단일 관찰 대상의 가시성 여부(boolean)를 의미한다.
    - useFadeIn의 인자가 있는 경우, 100ms의 간격으로 변화하는 boolean[]을 의미한다.

---
+++ src/hooks/useFadeIn.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import useIntersectionObserver from './useIntersectionObserver';
+
+export const useFadeIn = (triggers?: number) => {
+  const [isShow, setIsShow] = useState(false);
+  const [trigger, setTrigger] = useState<{ [x: number]: boolean }>();
+
+  const onIntersect: IntersectionObserverCallback = ([entry], observer) => {
+    if (entry.isIntersecting) {
+      setIsShow(true);
+      observer.disconnect();
+    }
+  };
+  const { setTarget } = useIntersectionObserver({ onIntersect });
+
+  useEffect(() => {
+    if (triggers) {
+      const initValue = {
+        ...[...Array(triggers)].map((_) => false),
+      };
+      setTrigger(initValue);
+    }
+  }, [triggers]);
+
+  useEffect(() => {
+    if (isShow && trigger) {
+      let triggerAnimation: NodeJS.Timeout;
+      Object.values(trigger).forEach((bool, index) => {
+        let idx = index as 0 | 1 | 2;
+        triggerAnimation = setTimeout(() => {
+          setTrigger((prev) => {
+            return { ...prev, [idx]: !bool };
+          });
+        }, index * 100);
+      });
+      return () => clearTimeout(triggerAnimation);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isShow]);
+
+  return {
+    ref: setTarget,
+    triggers: triggers ? Object.values(trigger ?? {}) : isShow,
+  };
+};
